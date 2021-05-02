b0704f5642d9962f12c5452a9ceaa3b9bb1afaa9
Rafael CNC <rafael.cechinato@pucpr.edu.br>
2021-05-02 17:20:15 -0300

    Alteracao userr store e model

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -5,11 +5,15 @@ require("dotenv/config");
 module.exports = {
     async store(req, res) {
         try {
+            let user = await User.findOne({ email: req.body.email});
+            if(user){
+                return res.status(400).json({ error:"Email cadastrado já existente" });
+            }
             req.body.password = await bcrypt.hash(req.body.password,parseInt(process.env.SALT_WORK_FACTOR));
             const newUser = await User.create(req.body);
             return res.json(newUser);
         } catch (error) {
-            return res.status(400).json({ error });
+            return res.status(400).json({ error:"Ocorreu um erro durante o cadastro tende mais tarde!" });
         }
     },
     async getUsers(req, res) {
--- src/models/User.js
+++ src/models/User.js
@@ -6,7 +6,7 @@ const UserSchema = new mongoose.Schema({
     email: {type: String,required: true,unique: true},
     password: {type: String,required: true},
     birthday: {type: Date,required: true},
-    cpf: {type: Number,required: true},
+    cpf: {type: String,required: true},
 
 }, { timestamps: {} });
 
